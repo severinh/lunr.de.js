9280a388e99cd8354bf2471af58d8d4361483c5b
Severin Heiniger <severinheiniger@gmail.com>
2013-09-05 19:14:29 +0200

    Reimplement the function 'lunr.de' such that it works like 'lunr'.

--- src/lunr.de.js
+++ src/lunr.de.js
@@ -1,14 +1,27 @@
 /**
- * lunr.de.js: A German language extension to lunr.js
+ * lunr.de.js: A German language extension for lunr.js
  * 
- * It includes a German stop word filter and a stemmer.
+ * Use the function 'lunr.de' exactly like the built-in convenience
+ * function 'lunr' for creating a new index. Compared to 'lunr', it
+ * adds a German stopword filter and stemmer to the pipeline by default.
+ *
+ * Example:
+ *
+ * var idx = lunr.de(function() {
+ *   this.field("body");
+ *   this.pipeline.add(...);
+ * });
+ *
+ * Author: Severin Heiniger <severinheiniger@gmail.com>
  */
 
-lunr.de = function () {
-  this.pipeline = new lunr.Pipeline;
-  this.pipeline.add(
-    lunr.de.stopWordFilter,
-    lunr.de.stemmer
-  );
-  return this;
+lunr.de = function (config) {
+  var idx = new lunr.Index;
+  idx.pipeline.add(lunr.de.stopWordFilter, lunr.de.stemmer);
+
+  if (config) {
+    config.call(idx, idx);
+  }
+
+  return idx;
 }
